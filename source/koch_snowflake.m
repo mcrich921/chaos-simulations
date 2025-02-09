e596bc4010add550c0dd320ab14a458fcb733bf8
mrichw <mrich@wesleyan.edu>
2025-02-09 15:16:27 -0500

    added koch snowflake files

--- source/koch_snowflake.m
+++ source/koch_snowflake.m
@@ -3,8 +3,11 @@ clc; clear; close all;
 % Define the number of recursion levels
 iterations = 4; % Change this for different detail levels
 
-saveto = 'koch_snowflake.gif';
-
+% True if want to save to a gif
+gif_mode = false;
+if gif_mode
+    saveto = 'koch_snowflake.gif';
+end
 % Define the initial equilateral triangle
 p1 = [0, 0];
 p2 = [1, 0];
@@ -18,19 +21,30 @@ for iter = 1:iterations
     cla;
     title(['Koch Snowflake - Iterations: ', num2str(iter)]);
 
+    % Color of lines - k for black
+    color = 'k';
+
     % Draw the three sides of the initial triangle recursively
-    draw_koch(p1, p3, iter);
-    draw_koch(p3, p2, iter);
-    draw_koch(p2, p1, iter);
+    % draw_koch(p1, p3, iter);
+    % draw_koch(p3, p2, iter);
+    % draw_koch(p2, p1, iter);
 
-    frame = getframe(gcf);
-    im = frame2im(frame);
-    [A, map] = rgb2ind(im, 256);
+    % For only one line
+    draw_koch(p1, p2, iter, color);
 
-    if iter == 1
-        imwrite(A, map, saveto, 'gif', 'LoopCount', Inf, 'DelayTime', 1.5)
-    else
-        imwrite(A, map, saveto, 'gif', 'WriteMode', 'append', 'DelayTime', 1.5)
+    % To control when to go to next
+    waitforbuttonpress();
+
+    if gif_mode
+        frame = getframe(gcf);
+        im = frame2im(frame);
+        [A, map] = rgb2ind(im, 256);
+    
+        if iter == 1
+            imwrite(A, map, saveto, 'gif', 'LoopCount', Inf, 'DelayTime', 1.5)
+        else
+            imwrite(A, map, saveto, 'gif', 'WriteMode', 'append', 'DelayTime', 1.5)
+        end
     end
 
     pause(1.5)
@@ -40,10 +54,10 @@ end
 hold off;
 
 % Function to recursively generate the Koch Snowflake
-function draw_koch(p1, p2, iter)
+function draw_koch(p1, p2, iter, color)
     if iter == 0
         % Base case: Draw a straight line
-        plot([p1(1), p2(1)], [p1(2), p2(2)], 'k', 'LineWidth', 1.5);
+        plot([p1(1), p2(1)], [p1(2), p2(2)], color, 'LineWidth', 1.5);
     else
         % Compute new points
         a = p1;
@@ -56,9 +70,9 @@ function draw_koch(p1, p2, iter)
         peak = (c - b) * rotation_matrix' + b;
         
         % Recursively draw the four segments
-        draw_koch(a, b, iter - 1);
-        draw_koch(b, peak, iter - 1);
-        draw_koch(peak, c, iter - 1);
-        draw_koch(c, p2, iter - 1);
+        draw_koch(a, b, iter - 1, color);
+        draw_koch(b, peak, iter - 1, color);
+        draw_koch(peak, c, iter - 1, color);
+        draw_koch(c, p2, iter - 1, color);
     end
 end
